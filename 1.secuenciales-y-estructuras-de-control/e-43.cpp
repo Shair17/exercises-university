5729a872670d8254dba1c15da40d710c79177bea
Shair17 <jimmy_jhair17@hotmail.com>
2020-08-07 12:37:12 -0500

    oops I forget

--- 1.secuenciales-y-estructuras-de-control/e-43.cpp
+++ 1.secuenciales-y-estructuras-de-control/e-43.cpp
@@ -10,3 +10,92 @@
  *      mujeres > 15 años                       12 - 16 g%
  *      hombres > 15 años                       14 - 18 g%
 */
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main() {
+  int edad;
+  char sexo, respuesta;
+  float nivelHemoblogina;
+
+  cout << "El paciente es menor de un año?";
+  cin >> respuesta;
+
+  if (respuesta == 's' || respuesta == 'S') {
+    cout << "Ingrese la edad del paciente en meses: ";
+    cin >> edad;
+
+    cout << "Nivel de hemoglobina del paciente: ";
+    cin >> nivelHemoblogina;
+
+    if (edad <= 1) {
+      if (nivelHemoblogina < 13) cout << "El paciente tiene anemia";
+      else cout << "El paciente no tiene anemia";
+    }
+
+    if (edad <= 6) {
+      if (nivelHemoblogina < 10) cout << "El paciente tiene anemia";
+      else cout << "El paciente no tiene anemia";
+    }
+
+    if (edad <= 12) {
+      if (nivelHemoblogina < 11) cout << "El paciente tiene anemia";
+      else cout << "El paciente no tiene anemia";
+    }
+  }
+
+  cout << "El paciente es mayor de un 1 año y menor de 15 años?";
+  cin >> respuesta;
+
+  if (respuesta == 's' || respuesta == 'S') {
+    cout << "Ingrese edad del paciente en meses ";
+    cin >> edad;
+
+    cout << "Ingrese nivel de hemoglobina ";
+    cin >> nivelHemoblogina;
+
+    if (edad <= 5) {
+      if (nivelHemoblogina < 11) cout << "El paciente tiene anemia";
+      else cout << "El paciente no tiene anemia";
+    }
+
+    if (edad <= 10) {
+      if (nivelHemoblogina < 12.6) cout << "El paciente tiene anemia";
+      else cout << "El paciente no tiene anemia";
+    }
+
+    if (edad <= 15) {
+      if (nivelHemoblogina < 13) cout << "El paciente tiene anemia";
+      else cout << "El paciente no tiene anemia";
+    }
+  }
+
+  cout << "El paciente es mayor de 15 años?";
+  cin >> respuesta;
+
+  if (respuesta == 's' || respuesta == 'S') {
+    cout << "Ingrese la edad del paciente en meses ";
+    cin >> edad;
+
+    cout << "Ingrese nivel de hemoglobina ";
+    cin >> nivelHemoblogina;
+
+    cout << "Ingrese el sexo del paciente (m | M: Masculino; f | F: Femenino) ";
+    cin >> sexo;
+
+    if (sexo == 'm' || sexo == 'M') {
+      if (nivelHemoblogina < 12) cout << "El paciente tiene anemia";
+      else cout << "El paciente no tiene anemia";
+    }
+
+    if (sexo == 'f' || sexo == 'F') {
+      if (nivelHemoblogina < 14) cout << "El paciente tiene anemia";
+      else cout << "El paciente no tiene anemia";
+    }
+  }
+
+  return 0;
+}
