dc7cdb15e46c65824cbe25a5af0aeaa8d28abaa6
Shair17 <jimmy_jhair17@hotmail.com>
2020-08-13 23:08:58 -0500

    aaa

--- 1.secuenciales-y-estructuras-de-control/e-54.cpp
+++ 1.secuenciales-y-estructuras-de-control/e-54.cpp
@@ -2,11 +2,21 @@
  * Una persona debe realizar un muestreo con 50 personas para determinar
  * el promedio de peso de los niños, jóvenes, adultos y viejos que existen
  * en su zona habitacional.
- * Se determinan las categorías con base  en la siguiente tabla:
+ * Se determinan las categorías con base en la siguiente tabla:
  * 
- *  CATEGORIA              EDAD
- * Niños                  0 - 12
- * Jóvenes                13 - 29
- * Adultos                30 - 59
- * Viejos                 60 - +
+ *  CATEGORÍA                 EDAD
+ *   Niños                   0 - 12
+ *   Jóvenes                13 - 29
+ *   Adultos                30 - 59
+ *   Viejos                 60 - +
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+
+  return 0;
+}
--- 1.secuenciales-y-estructuras-de-control/e-55.cpp
+++ 1.secuenciales-y-estructuras-de-control/e-55.cpp
@@ -10,6 +10,33 @@ using namespace std;
 
 int main() {
   int index = 1;
+  float totalP = 0, costoKilo, kilosComprados, subtotal, descuento, total;
+
+  cout << "Ingrese el valor de costo por kilo: ";
+  cin >> costoKilo;
+
+  while (index <= 15) {
+    cout << "Ingrese el valor de kilos comprados: ";
+    cin >> kilosComprados;
+
+    subtotal = kilosComprados * costoKilo;
+
+    if (kilosComprados > 10) {
+      descuento = subtotal * .1;
+    } else {
+      descuento = 0;
+    }
+
+    total = subtotal - descuento;
+
+    totalP += total;
+
+    cout << "Valor de descuento: " << descuento << endl;
+    cout << "Valor de subtotal: " << subtotal << endl;
+    cout << "Valor de total: " << total << endl;
+  }
+
+  cout << "Valor de total percibido: " << totalP;
 
   return 0;
 }
