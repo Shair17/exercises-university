d39bff943ab119e1f2bdf8b00d42761ca93330cf
Shair17 <jimmy_jhair17@hotmail.com>
2020-08-14 00:33:42 -0500

    the last ,_,

--- 1.secuenciales-y-estructuras-de-control/e-54.cpp
+++ 1.secuenciales-y-estructuras-de-control/e-54.cpp
@@ -16,7 +16,60 @@
 using namespace std;
 
 int main() {
-  
+  float pesoN, pesoJ, pesoA, pesoV, peso;
+  int ninhos = 0, jovenes = 0, adultos = 0, viejos = 0, edad, index = 1;
+  float promedioN, promedioJ, promedioA, promedioV;
+
+  while (index <= 50) {
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+
+    cout << "Ingrese su peso: ";
+    cin >> peso;
+
+    if (edad <= 12) {
+      ninhos += 1;
+      pesoN += peso;
+      promedioN = pesoN / ninhos;
+
+      cout << "Estás en la categoría de niños" << endl;
+      cout << "Total de niños: " << ninhos << endl;
+    }
+
+    if (edad >= 13 && edad <= 29) {
+      jovenes += 1;
+      pesoJ += peso;
+      promedioJ = pesoJ / jovenes;
+
+      cout << "Estás en la categoría de jovenes" << endl;
+      cout << "Total de jovenes: " << jovenes << endl;
+    }
+
+    if (edad >= 30 && edad <= 59) {
+      adultos += 1;
+      pesoA += peso;
+      promedioA = pesoJ / jovenes;
+
+      cout << "Estás en la categoría de adultos." << endl;
+      cout << "Total de adultos: " << adultos << endl;
+    }
+
+    if (edad >= 60) {
+      viejos += 1;
+      pesoV += peso;
+      promedioV = pesoV / viejos;
+
+      cout << "Estás en la categoría de viejos." << endl;
+      cout << "Total de viejos: " << viejos << endl;
+    }
+
+    index++;
+  }
+
+  cout << "El promedio de peso de niños es: " << promedioN;
+  cout << "El promedio de peso de jovenes es: " << promedioJ;
+  cout << "El promedio de peso de adultos es: " << promedioA;
+  cout << "El promedio de peso de viejos es: " << promedioV;
 
   return 0;
 }
